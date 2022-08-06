50abff046613cfdcfabc5782632df060df04c4d0
Alex Wesley <92955230+AlexWesleyy@users.noreply.github.com>
2022-08-06 11:04:59 -0300

    Adicionando funcionalidades

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+window.addEventListener('scroll', onScroll)
+
 function onScroll(){
+    showNavOnScroll()
+    showBackToButtonOnScroll()
+}
+function showNavOnScroll(){
     const elemento = document.getElementById("navigation");
     if (scrollY>40.79999923706055){
         elemento.classList.add("scroll");
     }else{
         elemento.classList.remove("scroll");
+    }
+}
+function showBackToButtonOnScroll(){
+    const elemento = document.getElementById("back-to-top");
+    if (scrollY>500){
+        elemento.classList.add("show");
+    }else{
+        elemento.classList.remove("show");
     }
 }
 function openMenu(){
@@ -23,4 +37,7 @@ ScrollReveal({
 #home .stats, 
 #services,
 #services .cabecario,
-#services .card`);
+#services .card,
+#about,
+#about header,
+#about .tela .content`);
